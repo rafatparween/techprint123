d91460ec57e20371f2066cce2f5ba64cb6d48065
rafat22 <rafat22@navvgurukul.org>
2024-10-28 23:23:18 +0530

    chat change

--- app/Jivochat.js
+++ app/Jivochat.js
@@ -6,7 +6,7 @@ const JivoChat = () => {
   useEffect(() => {
     const loadJivoChatScript = () => {
       const script = document.createElement('script');
-      script.src = "//code.jivosite.com/widget/kd9uAKn19v";
+      script.src = "//code.jivosite.com/widget/NXU896TaVh";
       script.async = true;
       document.body.appendChild(script);
       script.onload = () => {
